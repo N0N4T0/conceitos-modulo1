c0340b4be8cc241dbe4c00115691cbc3f22be4e7
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-05-26 21:58:10 -0300

    3 commit
    atualizando códigos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,96 +1,96 @@
-const express = require('express');
-const cors = require('cors');
-const { uuid, isUuid } = require('uuidv4');
+const express = require('express')
+const cors = require('cors')
+const {uuid, isUuid} = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
+app.use(express.json())
 
-app.use(express.json());
+function logRequests(request, response, next){
+    const { method, url } = request
 
-const projects = [];
+    const logLabel = `[${method.toUpperCase()}] ${url}` 
 
-function logRequests(request, response, next) {
-    const { method, url } = request;
-    
-    const logLabel = `[${method.toUpperCase()}] ${url}`;
+    console.log(logLabel)
 
-    console.time(logLabel);
-
-    next();
-
-    console.timeEnd(logLabel);
+    return next()
 }
 
-function validateProjectId(request, response, next) {
-    const { id } = request.params;
+function validateProjectId(request, response, next){
+    const { id } = request.params
 
-    if (!isUuid(id)) {
-        return response.status(400).json({ error: 'Invalid project Id' });
+    if(!isUuid(id)){
+        return response.status(400).json({error: "Invalid projec ID"})
     }
 
-    return next();
-};
+    return next()
+}
 
-app.use(logRequests);
-app.use('/projects/:id', validateProjectId);
+app.use(logRequests)
 
-//rotas
-app.get('/projects', (request, response) => {
+
+const projects = []
+
+app.get("/projects", (request, response) => {
     const { title } = request.query;
 
-    const results = title 
+    const results = title
         ? projects.filter(project => project.title.includes(title))
-        : projects; 
+        : projects
 
-    return response.json(results);
-});
-
-app.post('/projects', (request, response) => {
-    const { title, ower } = request.body;
+    if (results == 0) {
+        return response.status(404).json({error: "No project available"})
+    }
     
-    const project = { id: uuid(), title, ower };
+    return response.json(results)
+})
 
-    projects.push(project);
+app.post("/projects", (request, response) => {
+    const {title, owner} = request.body
 
-    return response.json(project);
-});
+    const project = {id: uuid(), title, owner}
 
-app.put('/projects/:id', (request, response) =>{
-    const { id } = request.params;
-    const { title, ower } = request.body;
+    projects.push(project)
 
-    const projectIndex = projects.findIndex(project => project.id == id);
+    return response.json(project)
+})
 
-    if (projectIndex < 0) {
-        return response.status(400).json({ error: 'Project not found!' })
+app.put("/projects/:id", validateProjectId, (request, response) => {
+    const { id } = request.params
+    const { title, owner } = request.body
+
+    const projectIndex = projects.findIndex(project => project.id == id)
+   
+    if(projectIndex < 0){
+        return response.status(400).json({error: "Project not found!"})
     }
 
     const project = {
         id,
         title,
-        ower,
-    };
-
-    projects[projectIndex] = project;
-
-    return response.json(project);
-});
-
-app.delete('/projects/:id', (request, response) => {
-    const { id } = request.params;
-   
-    const projectIndex = projects.findIndex(project => project.id == id);
-
-    if (projectIndex < 0) {
-        return response.status(400).json({ error: 'Project not found!' })
+        owner
     }
-   
-    projects.splice(projectIndex, 1);
 
-    return response.status(204).send();
-});
+    projects[projectIndex] = project
+
+    return response.json(project)
+})
+
+app.delete("/projects/:id", validateProjectId, (request, response) => {
+    const {id} = request.params
+
+    const projectIndex = projects.findIndex(project => project.id == id)
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: "Project not found"})
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
+})
 
 app.listen(3333, () => {
-    console.log('(---@---) Back-end started!');
-});
+    console.log('💯 ☢ Back-end started!')
+})
